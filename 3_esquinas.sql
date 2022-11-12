403a67e8ab71ec1e2a9448c9963acfde1c3d52bf
DanneryOrtega <65132432+DanneryOrtega@users.noreply.github.com>
2022-11-12 18:15:03 -0500

    bases de datos

---
+++ 3_esquinas.sql
@@ -0,0 +1,25 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         5.7.33 - MySQL Community Server (GPL)
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             11.2.0.6213
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Volcando datos para la tabla 3_esquinas.client: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+INSERT INTO `client` (`id`, `cedula_nit`, `nombre_completo`, `dirección`, `celular`) VALUES
+	(1, 1005884123, 'Salome Ortega', 'Obrero', '3153267844'),
+	(2, 1006889541, 'Julieta León', 'Prado', '3125697845');
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
